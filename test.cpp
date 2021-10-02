edc099ff04fb15606e32a6eb01ea9f07233050c5
epicrandy@outlook.com <rzuni013@hammer.cs.ucr.edu>
2021-10-02 13:01:19 -0700

    added 3 more test to test.cpp

--- test.cpp
+++ test.cpp
@@ -7,6 +7,28 @@ TEST(EchoTest, HelloWorld) {
     EXPECT_EQ("hello world", echo(3,test_val));
 }
 
+TEST(EchoTest, EmptyString) {
+    char* test_val[1]; test_val[0] = "./c-echo";
+    EXPECT_EQ("", echo(1,test_val));
+}
+
+TEST(EchoTest, WordIsBonkus) {
+	char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "Bonkus";
+	EXPECT_EQ("Bonkus", echo(2,test_val));
+}
+
+TEST(EchoTest, HaloInfinite) {
+    char* test_val[3]; test_val[0] = "./c-echo"; test_val[1] = "Halo"; test_val[2] = "Infinite";
+    EXPECT_EQ("Halo Infinite", echo(3,test_val));
+}
+
+TEST(EchoTest, RipAndTear) {
+	char* test_val[4]; test_val[0] = "./c-echo"; test_val[1] = "Rip"; test_val[2] = "and"; test_val[3] = "tear";
+	EXPECT_EQ("Rip and tear", echo(4,test_val));
+}
+
+
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
